5a660d706e816992ef576798a3ac7396da5e3329
Ben DiMattia <brdimattia@me.com>
2016-09-26 17:50:22 -0400

    Facebook Sdk Setup

---
+++ Befriend/FacebookWrapper.swift
@@ -0,0 +1,9 @@
+//
+//  FacebookWrapper.swift
+//  Befriend
+//
+//  Created by Ben DiMattia on 9/25/16.
+//  Copyright © 2016 MisMattia. All rights reserved.
+//
+
+import Foundation
---
+++ Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Befriend
+//
+//  Created by Ben DiMattia on 9/26/16.
+//  Copyright © 2016 MisMattia. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+
+#endif /* Bridging_Header_h */
