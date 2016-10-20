cef57d2286692ffcd8bd25e57bd274e82a362728
Danielle Misiaszek <dmisiaszek@students.stonehill.edu>
2016-10-20 16:00:37 -0400

    added chart

--- Befriend/SpotifyWrapper.swift
+++ Befriend/SpotifyWrapper.swift
@@ -13,6 +13,14 @@ import OAuthSwift
 class SpotifyWrapper{
     
     func followRequest(_ oauthswift: OAuth1Swift, screenName: String){
-    
+    //    var params = Dictionary<String, String>()
+//        params["follow"] = "true"
+  //      params["screen_name"] = screenName
+        let _ =  oauthswift.client.post("https://api.twitter.com/1.1/friendships/create.json", parameters: params,
+                                        success: { data, response in
+                                            let dataString = String(data: data, encoding: String.Encoding.utf8)
+                                            print("datastring", dataString)},
+                                        failure: { error in
+                                            print("the error is" , error)})
     }
 }
